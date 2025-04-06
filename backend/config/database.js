090a32c416dbb5f6b7b29002d4e282bed004b9d0
Aidil-hash <aidilfahmi060@gmail.com>
2025-04-07 00:56:35 +0800

    styled

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,12 @@
 // db.js
 
 const mongoose = require('mongoose');
+const DB_URI = process.env.DB_URI; // Replace with your database name
 
 const connectDB = async () => {
   try {
     // Replace the connection string with your local or Atlas URI
-    await mongoose.connect('mongodb+srv://onlineDB:roland-123@cluster0.5irue.mongodb.net/', {
+    await mongoose.connect(DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
