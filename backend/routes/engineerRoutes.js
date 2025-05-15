9901a14cd7e8b09b4dd0579ceb4381c7d92299c6
Aidil-hash <aidilfahmi060@gmail.com>
2025-05-15 05:52:29 +0000

    edited

--- backend/routes/engineerRoutes.js
+++ backend/routes/engineerRoutes.js
@@ -1,7 +1,7 @@
 // routes/engineerRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getAnalytics, getScanLogs, getAllScans } = require('../controllers/engineerController');
+const { getAnalytics, getScanLogs, getAllScans, assignLineToOperator, detachOperatorFromLine } = require('../controllers/engineerController');
 const { verifyToken } = require('../middleware/authMiddleware'); // Assumes you have token verification middleware
 
 // GET /api/engineer/analytics - Get analytics data for engineers
