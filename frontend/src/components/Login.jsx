ecf63ce09f03d7ffeb79c26138febf86e2bd0935
Aidil-hash <aidilfahmi060@gmail.com>
2025-06-08 15:55:54 +0800

    edited

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -98,7 +98,7 @@ export default function Login() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
-                placeholder="••••••••"
+                placeholder="Password"
               />
             </div>
 
