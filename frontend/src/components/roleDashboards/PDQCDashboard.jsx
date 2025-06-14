2a2854ffcec7eb09ee77c2cc60624204e10953cf
Aidil-hash <aidilfahmi060@gmail.com>
2025-06-14 08:20:19 +0800

    remove unused useEffect import from PDQCDashboard component

--- frontend/src/components/roleDashboards/PDQCDashboard.jsx
+++ frontend/src/components/roleDashboards/PDQCDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
   Box, Typography, Button, TextField, Table, TableBody,
   TableCell, TableContainer, TableHead, TableRow, Paper
