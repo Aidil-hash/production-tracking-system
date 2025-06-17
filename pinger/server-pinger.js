2f3a36ebf25020e3f1b01d260d17d624eff040dc
Aidil-hash <aidilfahmi060@gmail.com>
2025-06-17 08:50:33 +0000

    removed

--- pinger/server-pinger.js
+++
@@ -1,9 +0,0 @@
-import https from 'https';
-
-setInterval(() => {
-  https.get('https://project-roland.onrender.com/', (res) => {
-    console.log(`Pinged server. Status: ${res.statusCode}`);
-  }).on('error', (err) => {
-    console.error('Error pinging server:', err);
-  });
-}, 5 * 60 * 1000); // Every 5 minutes
